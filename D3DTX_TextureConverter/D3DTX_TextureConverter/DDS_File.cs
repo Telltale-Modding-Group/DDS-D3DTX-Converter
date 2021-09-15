89539c70d52e18f23f9bc2b102632f7d822f9412
changemymindpls <40375502+changemymindpls@users.noreply.github.com>
2021-09-14 20:47:33 -0500

    6vsm d3dtx header almost complete

--- D3DTX_TextureConverter/D3DTX_TextureConverter/DDS_File.cs
+++ D3DTX_TextureConverter/D3DTX_TextureConverter/DDS_File.cs
@@ -6,6 +6,34 @@ using DirectXTexNet;
 using D3DTX_TextureConverter.Utilities;
 using D3DTX_TextureConverter.DirectX;
 
+/*
+ * DXT1 - DXGI_FORMAT_BC1_UNORM / D3DFMT_DXT1
+ * DXT2 - D3DFMT_DXT2
+ * DXT3 - DXGI_FORMAT_BC2_UNORM / D3DFMT_DXT3
+ * DXT4 - D3DFMT_DXT4
+ * DXT5 - DXGI_FORMAT_BC3_UNORM / D3DFMT_DXT5
+ * ATI1
+ * ATI2 - DXGI_FORMAT_BC5_UNORM
+ * BC5U - 
+ * BC5S - DXGI_FORMAT_BC5_SNORM
+ * BC4U - DXGI_FORMAT_BC4_UNORM
+ * BC4S - DXGI_FORMAT_BC4_SNORM
+ * RGBG - DXGI_FORMAT_R8G8_B8G8_UNORM / D3DFMT_R8G8_B8G8
+ * GRGB - DXGI_FORMAT_G8R8_G8B8_UNORM / D3DFMT_G8R8_G8B8
+ * UYVY - D3DFMT_UYVY
+ * YUY2 - D3DFMT_YUY2
+ * DX10 - Any DXGI format
+ * 36 - DXGI_FORMAT_R16G16B16A16_UNORM / D3DFMT_A16B16G16R16
+ * 110 - DXGI_FORMAT_R16G16B16A16_SNORM / D3DFMT_Q16W16V16U16
+ * 111 - DXGI_FORMAT_R16_FLOAT / D3DFMT_R16F
+ * 112 - DXGI_FORMAT_R16G16_FLOAT / D3DFMT_G16R16F
+ * 113 - DXGI_FORMAT_R16G16B16A16_FLOAT / D3DFMT_A16B16G16R16F
+ * 114 - DXGI_FORMAT_R32_FLOAT / D3DFMT_R32F
+ * 115 - DXGI_FORMAT_R32G32_FLOAT / D3DFMT_G32R32F
+ * 116 - DXGI_FORMAT_R32G32B32A32_FLOAT / D3DFMT_A32B32G32R32F
+ * 117 - D3DFMT_CxV8U8
+*/
+
 namespace D3DTX_TextureConverter
 {
     /// <summary>
@@ -29,9 +57,39 @@ namespace D3DTX_TextureConverter
 
         public DDS_File(D3DTX_File d3dtx)
         {
+            header = DDS_Functions.GetPresetHeader();
             header.dwWidth = (uint)d3dtx.T3TextureBase.mWidth;
             header.dwHeight = (uint)d3dtx.T3TextureBase.mHeight;
             header.dwMipMapCount = (uint)d3dtx.T3TextureBase.mNumMipLevelsAllocated;
+            header.dwDepth = (uint)d3dtx.T3TextureBase.mDepth;
+            
+            switch(d3dtx.T3TextureBase.mSurfaceFormat)
+            {
+                default:
+                    header.ddspf.dwFourCC = uint.Parse("DXT1");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_DXT1:
+                    header.ddspf.dwFourCC = uint.Parse("DXT1");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_DXT3:
+                    header.ddspf.dwFourCC = uint.Parse("DXT3");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_DXT5:
+                    header.ddspf.dwFourCC = uint.Parse("DXT5");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_BC5:
+                    header.ddspf.dwFourCC = uint.Parse("BC5U");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_BC4:
+                    header.ddspf.dwFourCC = uint.Parse("BC4U");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_RGBA16:
+                    header.ddspf.dwFourCC = uint.Parse("36");
+                    break;
+                case Telltale.T3SurfaceFormat.eSurface_RGBA16F:
+                    header.ddspf.dwFourCC = uint.Parse("110");
+                    break;
+            }
         }
 
         /// <summary>

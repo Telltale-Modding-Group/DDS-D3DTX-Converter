17efc2bcc4064a33fab61628aedf92f7d848c399
Mawrak <jenialuk@rambler.ru>
2023-01-16 22:40:04 -0800

    Added console UI. Made a build.

--- D3DTX_Converter/D3DTX_Converter/ProgramDebug/Program_Debug.cs
+++ D3DTX_Converter/D3DTX_Converter/ProgramDebug/Program_Debug.cs
@@ -51,19 +51,40 @@ namespace D3DTX_Converter.ProgramDebug
         /// </summary>
         /// <param name="args"></param>
         public static void Main(string[] args)
-        {
+        { 
             ConsoleFunctions.SetConsoleColor(ConsoleColor.Blue, ConsoleColor.White);
             Console.WriteLine("D3DTX to DDS Texture Converter");
 
             ConsoleFunctions.SetConsoleColor(ConsoleColor.DarkGreen, ConsoleColor.White);
-            Console.WriteLine("Enter 1 for d3dtx to dds. Enter 2 for d3dtx to dds.");
+            Console.WriteLine("Enter 1 for d3dtx to dds. Enter 2 for dds to d3dtx. Enter 3 for extra - may not work correctly.");
 
             //texture folder path (containing the path to the textures to be converted)
             string val = Console.ReadLine();
             int a = Convert.ToInt32(val);
-            if (a == 1) { Program_D3DTX_TO_DDS.Execute(); return; }
-            else if (a == 2) { Program_DDS_TO_D3DTX.Execute(); return; }
-            else { ConsoleFunctions.SetConsoleColor(ConsoleColor.Red, ConsoleColor.White); ;  Console.WriteLine("Invalid Input."); return; }
+            //main
+            if (a == 1) { Program_D3DTX_TO_DDS.Execute(); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+            else if (a == 2) { Program_DDS_TO_D3DTX.Execute(); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+            else if (a == 3)
+            {
+                
+                Console.WriteLine("Enter 1 for dds to png. \nEnter 2 for dds to bmp. \nEnter 3 for dds to tiff. \nEnter 4 for dds to jpg.");
+                Console.WriteLine("Enter 3 for png to dds. \nEnter 6 for bmp to dds. \nEnter 7 for tiff to dds. \nEnter 8 for jpg to dds.");
+                string val2 = Console.ReadLine();
+                int b = Convert.ToInt32(val2);
+                //dds intoelse if (a == 2)  standard images
+                if (b == 1) { Program_DDS_TO_PNG.Execute(fixes_dds_to_generic); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else if (b == 2) { Program_DDS_TO_BMP.Execute(fixes_dds_to_generic); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else if (b == 3) { Program_DDS_TO_TIFF.Execute(fixes_dds_to_generic); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else if (b == 4) { Program_DDS_TO_JPEG.Execute(fixes_dds_to_generic); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                //standard images into dds
+                else if (b == 5) { Program_PNG_TO_DDS.Execute(fixes_generic_to_dds); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else if (b == 6) { Program_BMP_TO_DDS.Execute(fixes_generic_to_dds); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else if (b == 7) { Program_TIFF_TO_DDS.Execute(fixes_generic_to_dds); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else if (b == 8) { Program_JPEG_TO_DDS.Execute(fixes_generic_to_dds); Console.WriteLine("Conversion finished. Program will close automatically."); Thread.Sleep(6000); Thread.Sleep(6000); return; }
+                else { ConsoleFunctions.SetConsoleColor(ConsoleColor.Red, ConsoleColor.White); ; Console.WriteLine("Invalid Input. Program will close automatically."); Thread.Sleep(6000); return; }
+            }
+
+            else { ConsoleFunctions.SetConsoleColor(ConsoleColor.Red, ConsoleColor.White); ; Console.WriteLine("Invalid Input. Program will close automatically."); Thread.Sleep(6000); return; }
 
 
 
@@ -72,16 +93,16 @@ namespace D3DTX_Converter.ProgramDebug
             //if (mode_dds_to_d3dtx) { Program_DDS_TO_D3DTX.Execute(); return; }
 
             //dds into standard images
-            if (mode_dds_to_png) { Program_DDS_TO_PNG.Execute(fixes_dds_to_generic); return; }
-            if (mode_dds_to_bmp) { Program_DDS_TO_BMP.Execute(fixes_dds_to_generic); return; }
-            if (mode_dds_to_tiff) { Program_DDS_TO_TIFF.Execute(fixes_dds_to_generic); return; }
-            if (mode_dds_to_jpeg) { Program_DDS_TO_JPEG.Execute(fixes_dds_to_generic); return; }
+            //if (mode_dds_to_png) { Program_DDS_TO_PNG.Execute(fixes_dds_to_generic); return; }
+            //if (mode_dds_to_bmp) { Program_DDS_TO_BMP.Execute(fixes_dds_to_generic); return; }
+            //if (mode_dds_to_tiff) { Program_DDS_TO_TIFF.Execute(fixes_dds_to_generic); return; }
+            //if (mode_dds_to_jpeg) { Program_DDS_TO_JPEG.Execute(fixes_dds_to_generic); return; }
 
             //standard images into dds
-            if (mode_png_to_dds) { Program_PNG_TO_DDS.Execute(fixes_generic_to_dds); return; }
-            if (mode_bmp_to_dds) { Program_BMP_TO_DDS.Execute(fixes_generic_to_dds); return; }
-            if (mode_tiff_to_dds) { Program_TIFF_TO_DDS.Execute(fixes_generic_to_dds); return; }
-            if (mode_jpeg_to_dds) { Program_JPEG_TO_DDS.Execute(fixes_generic_to_dds); return; }
+            //if (mode_png_to_dds) { Program_PNG_TO_DDS.Execute(fixes_generic_to_dds); return; }
+            //if (mode_bmp_to_dds) { Program_BMP_TO_DDS.Execute(fixes_generic_to_dds); return; }
+            //if (mode_tiff_to_dds) { Program_TIFF_TO_DDS.Execute(fixes_generic_to_dds); return; }
+            //if (mode_jpeg_to_dds) { Program_JPEG_TO_DDS.Execute(fixes_generic_to_dds); return; }
         }
     }
 }

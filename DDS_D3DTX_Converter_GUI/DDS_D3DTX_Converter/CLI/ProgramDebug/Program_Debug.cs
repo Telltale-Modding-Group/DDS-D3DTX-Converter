8826d9dcd11195cbecfc8900678c41d7facbefe7
DarkShadow <ivanivov.p@gmail.com>
2024-05-23 18:06:48 +0300

    Add debug CLI

--- DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/CLI/ProgramDebug/Program_Debug.cs
+++ DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/CLI/ProgramDebug/Program_Debug.cs
@@ -1,4 +1,6 @@
-﻿//DEPRECATED
+﻿using System;
+using D3DTX_Converter.Main;
+
 namespace D3DTX_Converter.ProgramDebug
 {
     public static class Program_Debug
@@ -6,28 +8,25 @@ namespace D3DTX_Converter.ProgramDebug
         //----------------------CONVERSION MODES----------------------
 
         /*
-         * HOW TO USE: Change the value of a certain mode to 'true' to enable the desired mode.
-         * ONLY 1 SHOULD BE 'true' AT A TIME
+         * HOW TO USE: Change the boolean value of a certain mode to 'true' to enable the desired mode.
+         * Read the output for error messages. 
         */
 
         //main conversion to and from d3dtx
-        public static bool mode_d3dtx_to_dds = true; //.d3dtx ---> .dds
-        public static bool mode_dds_to_d3dtx = false; //.dds ---> .d3dtx
 
         public static bool fixes_generic_to_dds = true; //FIXES normal maps and swizzle channels
         public static bool fixes_dds_to_generic = true; //FIXES normal maps and swizzle channels
 
-        //dds into standard images
-        public static bool mode_dds_to_png = false; //.dds ---> .png
-        public static bool mode_dds_to_bmp = false; //.dds ---> .bmp
-        public static bool mode_dds_to_tiff = false; //.dds ---> .tiff
-        public static bool mode_dds_to_jpeg = false; //.dds ---> .jpeg
+        public const int D3DTX_TO_DDS = 0; //.d3dtx ---> .dds
+        public const int DDS_TO_D3DTX = 1; //.dds ---> .d3dtx
+        public const int DDS_TO_OTHERS = 2; //.dds ---> .png/.jpg etc.
+        public const int OTHERS_TO_DDS = 3; //.png/.jpg etc. ---> .dds
 
-        //standard images into dds
-        public static bool mode_png_to_dds = false; //.png ---> .dds
-        public static bool mode_bmp_to_dds = false; //.bmp ---> .dds
-        public static bool mode_tiff_to_dds = false; //.tiff ---> .dds
-        public static bool mode_jpeg_to_dds = false; //.jpeg ---> .dds
+        //dds into standard images
+        public const int DDS_TO_PNG = 0; //.dds <---> .png
+        public const int DDS_TO_JPEG = 1; //.dds <---> .jpg
+        public const int DDS_TO_TIFF = 2; //.dds <---> .tiff
+        public const int DDS_TO_BMP = 3; //.dds <---> .bmp
 
         //----------------------CONVERSION MODES END----------------------
 
@@ -37,21 +36,119 @@ namespace D3DTX_Converter.ProgramDebug
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            //main conversion to and from d3dtx
-            if (mode_d3dtx_to_dds) { Program_D3DTX_TO_DDS.Execute(); return; }
-            if (mode_dds_to_d3dtx) { Program_DDS_TO_D3DTX.Execute(); return; }
+            ConsoleFunctions.SetConsoleColor(ConsoleColor.Blue, ConsoleColor.White);
+            Console.WriteLine("Telltale Texture Mod Tool v2.4.0 CLI");
 
-            //dds into standard images
-            if (mode_dds_to_png) { Program_DDS_TO_PNG.Execute(fixes_dds_to_generic); return; }
-            if (mode_dds_to_bmp) { Program_DDS_TO_BMP.Execute(fixes_dds_to_generic); return; }
-            if (mode_dds_to_tiff) { Program_DDS_TO_TIFF.Execute(fixes_dds_to_generic); return; }
-            if (mode_dds_to_jpeg) { Program_DDS_TO_JPEG.Execute(fixes_dds_to_generic); return; }
+            ConsoleFunctions.SetConsoleColor(ConsoleColor.DarkGray, ConsoleColor.White);
+            Console.WriteLine("Enter the file conversion mode:");
+            int fileConversionMode = Program_Shared.GetFileConversionMode();
 
-            //standard images into dds
-            if (mode_png_to_dds) { Program_PNG_TO_DDS.Execute(fixes_generic_to_dds); return; }
-            if (mode_bmp_to_dds) { Program_BMP_TO_DDS.Execute(fixes_generic_to_dds); return; }
-            if (mode_tiff_to_dds) { Program_TIFF_TO_DDS.Execute(fixes_generic_to_dds); return; }
-            if (mode_jpeg_to_dds) { Program_JPEG_TO_DDS.Execute(fixes_generic_to_dds); return; }
+            ConsoleFunctions.SetConsoleColor(ConsoleColor.DarkGray, ConsoleColor.White);
+            Console.WriteLine("Do you want to convert whole a folder? Write Y for yes or N for no.");
+            bool bulkConvert = Program_Shared.GetBulkConversionBoolean();
+
+            string path;
+            if (bulkConvert)
+            {
+                Console.WriteLine("Enter the folder path with the textures to convert.");
+                path = Program_Shared.GetFolderPathFromUser();
+            }
+            else
+            {
+                Console.WriteLine("Enter the texture file path to convert.");
+                path = Program_Shared.GetFilePathFromUser();
+            }
+
+            Console.WriteLine("Enter the file path you wish to convert.");
+            string resultPath = Program_Shared.GetFolderPathFromUser();
+
+            string originalFormat;
+            string formatToConvert;
+            int d3dtxConversionMode = 0;
+
+            if (fileConversionMode == D3DTX_TO_DDS)
+            {
+                originalFormat = "dds";
+                formatToConvert = "d3dtx";
+
+                Console.WriteLine("Enter the D3DTX conversion mode:");
+                d3dtxConversionMode = Program_Shared.GetD3DTXConversionMode();
+            }
+            else if (fileConversionMode == DDS_TO_D3DTX)
+            {
+                originalFormat = "dds";
+                formatToConvert = "d3dtx";
+            }
+            else if (fileConversionMode == DDS_TO_OTHERS)
+            {
+                originalFormat = "dds";
+
+                Console.WriteLine("Enter the other file type mode:");
+                int fileType = Program_Shared.GetOthersConversionMode();
+                formatToConvert = fileType switch
+                {
+                    DDS_TO_PNG => "png",
+                    DDS_TO_JPEG => "jpeg",
+                    DDS_TO_TIFF => "tif",
+                    DDS_TO_BMP => "bmp",
+                    _ => "png",
+                };
+            }
+            else
+            {
+                int fileType = Program_Shared.GetOthersConversionMode();
+                originalFormat = fileType switch
+                {
+                    DDS_TO_PNG => "png",
+                    DDS_TO_JPEG => "jpeg",
+                    DDS_TO_TIFF => "tif",
+                    DDS_TO_BMP => "bmp",
+                    _ => "png",
+                };
+                formatToConvert = "dds";
+            }
+
+            try
+            {
+                ConsoleFunctions.SetConsoleColor(ConsoleColor.Black, ConsoleColor.Green);
+                Console.WriteLine("Conversion Starting...");
+                if (bulkConvert)
+                {
+                    Converter_CLI.ConvertBulk(path, resultPath, originalFormat, formatToConvert, (D3DTXConversionType)d3dtxConversionMode);
+                }
+                else
+                {
+                    if (originalFormat == "d3dtx")
+                    {
+                        Converter_CLI.ConvertTextureFromD3DtxToDds(path, resultPath, (D3DTXConversionType)d3dtxConversionMode);
+                    }
+                    else if (originalFormat == "dds")
+                    {
+                        if (formatToConvert != "d3dtx")
+                        {
+                            Converter_CLI.ConvertTextureFromD3DtxToDds(path, resultPath, (D3DTXConversionType)d3dtxConversionMode);
+                        }
+                        else
+                        {
+                            Converter_CLI.ConvertTextureFromDdsToOthersAsync(path, resultPath, formatToConvert, fixes_dds_to_generic);
+                        }
+                    }
+                    else
+                    {
+                        Converter_CLI.ConvertTextureFileFromOthersToDds(path, resultPath, fixes_generic_to_dds);
+                    }
+                }
+                ConsoleFunctions.SetConsoleColor(ConsoleColor.Black, ConsoleColor.Green);
+                Console.WriteLine("Conversion Finished.");
+                Console.ResetColor();
+            }
+            catch (Exception e)
+            {
+                ConsoleFunctions.SetConsoleColor(ConsoleColor.Black, ConsoleColor.Red);
+                Console.WriteLine("ERROR");
+                Console.WriteLine(e.Message); //notify the user we found x amount of bmp files in the array
+                Console.ResetColor();
+            }
         }
     }
 }
--- DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/CLI/ProgramRelease/Program_Release.cs
+++ DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/CLI/ProgramRelease/Program_Release.cs
@@ -1,44 +1,43 @@
 ﻿using System;
 using System.IO;
 
-namespace D3DTX_Converter.ProgramRelease
+namespace D3DTX_Converter.ProgramRelease;
+
+public static class Program_Release
 {
-    public static class Program_Release
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        if (args.Length == 0)
         {
-            if (args.Length == 0)
+            Console.WriteLine("Please Write In Arguments");
+            return;
+        }
+
+        ArgumentInputPath argumentInputPath;
+        ArgumentMode argumentMode;
+        ArgumentOutputPath argumentOutputPath;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            string arg = args[i];
+
+            if (arg.Equals(ArgumentInputPath.Keyword) && (i + 1) > args.Length)
             {
-                Console.WriteLine("Please Write In Arguments");
-                return;
+                argumentInputPath = new(args);
             }
 
-            ArgumentInputPath argumentInputPath;
-            ArgumentMode argumentMode;
-            ArgumentOutputPath argumentOutputPath;
-
-            for(int i = 0; i < args.Length; i++)
+            if (arg.Equals(ArgumentMode.Keyword) && (i + 1) > args.Length)
             {
-                string arg = args[i];
-
-                if (arg.Equals(ArgumentInputPath.Keyword) && (i + 1) > args.Length)
-                {
-                    argumentInputPath = new(args);
-                }
-
-                if (arg.Equals(ArgumentMode.Keyword) && (i + 1) > args.Length)
-                {
-                    argumentMode = new(args);
-                }
+                argumentMode = new(args);
             }
+        }
 
-            string inputDirectory = args[0];
+        string inputDirectory = args[0];
 
-            if (Directory.Exists(inputDirectory) == false)
-            {
-                Console.WriteLine("Invalid Input Directory");
-                return;
-            }
+        if (Directory.Exists(inputDirectory) == false)
+        {
+            Console.WriteLine("Invalid Input Directory");
+            return;
         }
     }
 }

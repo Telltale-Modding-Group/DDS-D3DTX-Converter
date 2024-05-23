f14aff11113f43c8139854db92190326d8ae6eb6
DarkShadow <ivanivov.p@gmail.com>
2024-05-23 17:28:11 +0300

    Refactor MessageBoxes

--- DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/Utilities/MessageBoxes.cs
+++ DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/Utilities/MessageBoxes.cs
@@ -5,66 +5,99 @@ using Avalonia.Platform;
 using MsBox.Avalonia.Dto;
 using MsBox.Avalonia.Enums;
 
-namespace DDS_D3DTX_Converter_GUI.Utilities
+namespace DDS_D3DTX_Converter_GUI.Utilities;
+
+/// <summary>
+/// A helper class for the dialog messages. 
+/// </summary>
+public static class MessageBoxes
 {
+    public const string ASSETS_DIRECTORY = "avares://DDS_D3DTX_Converter/Assets/";
+    public const string APP_ICON = ASSETS_DIRECTORY + "main_icon.ico";
+
     /// <summary>
-    /// A helper class for the dialog messages. 
+    /// Creates and returns the error dialog.
     /// </summary>
-    public static class MessageBoxes
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public static MessageBoxStandardParams GetErrorBox(string message)
     {
-        /// <summary>
-        /// Creates and returns the error dialog.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        public static MessageBoxStandardParams GetErrorBox(string message)
+        var bitmap = new Bitmap(AssetLoader.Open(new Uri(APP_ICON)));
+
+        return new MessageBoxStandardParams
         {
-            var assetsUri = new Uri("avares://DDS_D3DTX_Converter/Assets/");
-            var bitmap = new Bitmap(AssetLoader.Open(new Uri(assetsUri + "main_icon.ico")));
+            ButtonDefinitions = ButtonEnum.Ok,
+            ContentTitle = "Error",
+            ContentMessage = message,
+            Icon = Icon.Error,
+            WindowStartupLocation = WindowStartupLocation.CenterOwner,
+            WindowIcon = new WindowIcon(bitmap),
+            CanResize = false,
+            MaxWidth = 500,
+            MaxHeight = 800,
+            SizeToContent = SizeToContent.WidthAndHeight,
+            ShowInCenter = true,
+            Topmost = false,
+            EnterDefaultButton = ClickEnum.Ok,
+            EscDefaultButton = ClickEnum.Ok
+        };
+    }
 
-            return new MessageBoxStandardParams
-            {
-                ButtonDefinitions = ButtonEnum.Ok,
-                ContentTitle = "Error",
-                ContentMessage = message,
-                Icon = Icon.Error,
-                WindowStartupLocation = WindowStartupLocation.CenterOwner,
-                WindowIcon = new WindowIcon(bitmap),
-                CanResize = false,
-                MaxWidth = 500,
-                MaxHeight = 800,
-                SizeToContent = SizeToContent.WidthAndHeight,
-                ShowInCenter = true,
-                Topmost = false,
-            };
-        }
+    /// <summary>
+    /// Creates and returns the confirmation dialog.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public static MessageBoxStandardParams GetConfirmationBox(string message)
+    {
+        var bitmap = new Bitmap(AssetLoader.Open(new Uri(APP_ICON)));
 
-        /// <summary>
-        /// Creates and returns the confirmation dialog.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        public static MessageBoxStandardParams GetConfirmationBox(string message)
+        return new MessageBoxStandardParams
         {
-            var assetsUri = new Uri("avares://DDS_D3DTX_Converter/Assets/");
-            var bitmap = new Bitmap(AssetLoader.Open(new Uri(assetsUri + "main_icon.ico")));
+            ButtonDefinitions = ButtonEnum.YesNoCancel,
+            EnterDefaultButton = ClickEnum.Cancel,
+            EscDefaultButton = ClickEnum.Cancel,
+            ContentTitle = "Confirm",
+            ContentMessage = message,
+            Icon = Icon.Question,
+            WindowStartupLocation = WindowStartupLocation.CenterOwner,
+            WindowIcon = new WindowIcon(bitmap),
+            CanResize = false,
+            MaxWidth = 500,
+            MaxHeight = 800,
+            SizeToContent = SizeToContent.WidthAndHeight,
+            ShowInCenter = true,
+            Topmost = false,
+        };
+    }
 
-            return new MessageBoxStandardParams
-            {
-                ButtonDefinitions = ButtonEnum.YesNoCancel,
-                EnterDefaultButton = ClickEnum.Cancel,
-                ContentTitle = "Confirm",
-                ContentMessage = message,
-                Icon = Icon.Question,
-                WindowStartupLocation = WindowStartupLocation.CenterOwner,
-                WindowIcon = new WindowIcon(bitmap),
-                CanResize = false,
-                MaxWidth = 500,
-                MaxHeight = 800,
-                SizeToContent = SizeToContent.WidthAndHeight,
-                ShowInCenter = true,
-                Topmost = false,
-            };
-        }
+    /// <summary>
+    /// Creates and returns the confirmation dialog.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public static MessageBoxStandardParams GetDebugInformationBox(string message)
+    {
+        var bitmap = new Bitmap(AssetLoader.Open(new Uri(APP_ICON)));
+
+        return new MessageBoxStandardParams
+        {
+            ButtonDefinitions = ButtonEnum.Ok,
+            EnterDefaultButton = ClickEnum.Ok,
+            EscDefaultButton = ClickEnum.Ok,
+            ContentTitle = "Texture Debug Information",
+            ContentMessage = message,
+            Icon = Icon.Info,
+            WindowStartupLocation = WindowStartupLocation.CenterOwner,
+            WindowIcon = new WindowIcon(bitmap),
+            CanResize = false,
+            FontFamily = "Consolas",
+            MaxWidth = 1000,
+            MaxHeight = 700,
+            SizeToContent = SizeToContent.WidthAndHeight,
+            ShowInCenter = true,
+            Topmost = false,
+            ContentHeader = "Texture Debug Information:"
+        };
     }
 }

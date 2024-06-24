bbcdc7b30c26cc9a7a798c0f70e6a03e1048907b
DarkShadow <ivanivov.p@gmail.com>
2024-06-24 21:41:59 +0300

    Update Main_Shared.cs

--- DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/Main/Main_Shared.cs
+++ DDS_D3DTX_Converter_GUI/DDS_D3DTX_Converter/Main/Main_Shared.cs
@@ -2,16 +2,18 @@
 
 public class Main_Shared
 {
-    //file extensions
+    // File extensions
     public static string d3dtxExtension = ".d3dtx";
     public static string ddsExtension = ".dds";
+    public static string ktxExtension = ".ktx";
+    public static string ktx2Extension = ".ktx2";
     public static string jsonExtension = ".json";
 
     public static string bmpExtension = ".bmp";
     public static string pngExtension = ".png";
-    public static string tgaExtension = ".tga";
-    public static string jpgExtension = ".jpg";
     public static string jpegExtension = ".jpeg";
-    public static string tifExtension = ".tif";
+    public static string jpgExtension = ".jpg";
     public static string tiffExtension = ".tiff";
+    public static string tifExtension = ".tif";
+    public static string tgaExtension = ".tga";
 }
